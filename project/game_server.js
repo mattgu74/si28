327fee5e8cd1e634ab12a616e460db8352578331
Matthieu Guffroy <mattgu74@gmail.com>
2013-10-22 10:34:01 +0200

    Les objets se promenent d'un ecran à l'autre

--- project/game_server.js
+++ project/game_server.js
@@ -1,10 +1,43 @@
+function mod(a, b) {
+    console.log(a, b);
+    if (a>=b) {
+        return a - b;
+    }
+    if (a<0) {
+        return b - 1;
+    }
+    return a;
+}
+
 
 function run(io) {
+    var clients = new Array();
+
+    function get_right(clients, socket) {
+        index = clients.indexOf(socket) + 1;
+        index = mod(index, clients.length);
+        console.log(index);
+        return clients[index];
+    }
+    
+    function get_left(clients, socket) {
+        index = clients.indexOf(socket) - 1;
+        index = mod(index, clients.length);
+        console.log(index);
+        return clients[index];
+    }
+
     io.sockets.on('connection', function (socket) {
       socket.on('set nickname', function (name) {
         socket.set('nickname', name, function () {
           socket.emit('ready');
+          clients.push(socket);
         });
+      });
+      
+      socket.on('disconnect', function () {
+        console.log('User disconnect ! ');
+        clients.splice(clients.indexOf(socket), 1);
       });
 
       socket.on('msg', function (data) {
@@ -14,6 +47,35 @@ function run(io) {
           socket.broadcast.emit('msg', {name: name, msg: data});
         });
       });
+      
+      socket.on('obj_right', function (data) {
+        socket.get('nickname', function (err, name) {
+          console.log('Obj_right by ', name);
+          get_right(clients, socket).emit('obj', data);
+        });
+      });
+      
+      socket.on('obj_left', function (data) {
+        socket.get('nickname', function (err, name) {
+          console.log('Obj_left by ', name);
+          get_left(clients, socket).emit('obj', data);
+        });
+      });
+      
+      socket.on('obj_top', function (data) {
+        socket.get('nickname', function (err, name) {
+          console.log('Obj_top by ', name);
+          get_right(clients, socket).emit('obj', data);
+        });
+      });
+      
+      socket.on('obj_bottom', function (data) {
+        socket.get('nickname', function (err, name) {
+          console.log('Obj_bottom by ', name);
+          get_left(clients, socket).emit('obj', data);
+        });
+      });
+      
     });
 }
 
---
+++ project/public/javascripts/animation.js
@@ -0,0 +1,99 @@
+function min(a,b) {if(a<b) {return a;} else {return b;}}
+function max(a,b) {if(a>b) {return a;} else {return b;}}
+function abs(a) {if(a<0) {return -a;} else {return a;}}
+
+  var lastDist = 0;
+  var startScale = 1;
+  var activeShape = null;
+
+  function getDistance(p1, p2) {
+    return Math.sqrt(Math.pow((p2.x - p1.x), 2) + Math.pow((p2.y - p1.y), 2));
+  }
+  var stage = new Kinetic.Stage({
+    container: 'container',
+    width: window.innerWidth,
+    height: window.innerHeight,
+    x: 286,
+    y: 100,
+    offset: [289, 100]
+  });
+
+  window.onresize = function() {
+    stage.setWidth(window.innerWidth);
+    stage.setHeight(window.innerHeight);
+  };
+
+  var layer = new Kinetic.Layer();
+
+  var obj1 = new Kinetic.RegularPolygon({
+    x: 190,
+    y: stage.getHeight() / 3,
+    sides: 3,
+    radius: 80,
+    fill: 'green',
+    stroke: 'black',
+    strokeWidth: 4,
+    draggable: true,
+    name: 'triangle',
+    last_x: 190,
+    last_y: stage.getHeight() / 3
+  });
+  var obj2 = new Kinetic.Circle({
+    x: 380,
+    y: stage.getHeight() / 3,
+    radius: 70,
+    fill: 'red',
+    stroke: 'black',
+    strokeWidth: 4,
+    draggable: true,
+    name: 'circle',
+    last_x: 380,
+    last_y: stage.getHeight() / 3
+  });
+
+  layer.add(obj1);
+  layer.add(obj2);
+  stage.add(layer);
+
+  var anim = new Kinetic.Animation(function(frame) {
+    for (var i=0;i<layer.children.length;i++) {
+      obj = layer.children[i];
+      var dist_x = max(min(obj.getAttr('x') - obj.getAttr('last_x'), 10), -10);
+      var dist_y = max(min(obj.getAttr('y') - obj.getAttr('last_y'), 10), -10);
+      obj.setAttr('last_x', obj.getAttr('x'));
+      obj.setAttr('last_y', obj.getAttr('y'));
+      if(abs(dist_x) > 1 || abs(dist_y) > 1) {
+        obj.move(dist_x, dist_y);
+      }
+
+      if(obj.getAttr('x') > stage.getWidth()) {
+        // On traverse l'écran a droite
+        obj.setAttr('last_x', obj.getAttr('x') - stage.getWidth());
+        obj.move(dist_x - stage.getWidth(), dist_y);
+        send_object(obj, "right");
+        i-=1;
+      } else if(obj.getAttr('x') < 0) {
+        // on traverse l'écran a gauche
+        obj.setAttr('last_x', obj.getAttr('x') + stage.getWidth());
+        obj.move(dist_x + stage.getWidth(), dist_y);
+        send_object(obj, "left");
+        i-=1;
+      } else if(obj.getAttr('y') > stage.getHeight()) {
+        // on traverse en bas
+        obj.setAttr('last_y', obj.getAttr('y') - stage.getHeight());
+        obj.move(dist_x, dist_y - stage.getHeight());
+        send_object(obj, "bottom");
+        i-=1;
+      } else if(obj.getAttr('y') < 0) {
+        // on traverse en haut
+        obj.setAttr('last_y', obj.getAttr('y') + stage.getHeight());
+        obj.move(dist_x, dist_y + stage.getHeight());
+        send_object(obj, "top");
+        i-=1;
+      }
+
+    }
+  }, layer);
+
+  anim.start();
+
