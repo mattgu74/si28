c623cb1acc8ec8a26a1198efd3147d5d0b7e66d8
Juliette Lima <lima.juliette@laposte.net>
2013-12-10 01:03:48 +0100

    Shape animations with some bugs left

--- project/public/javascripts/shape.js
+++ project/public/javascripts/shape.js
@@ -65,7 +65,8 @@ var Shape = function(config) {
   }
 
   this.loadAnimation = function() {
-    var arc = new Kinetic.Shape({
+    /* CIRCLE DESTROY ANIMATION */
+    /* var arc = new Kinetic.Shape({
       drawFunc: function(context) {
           var x = 0;
           var y = 0;
@@ -93,7 +94,121 @@ var Shape = function(config) {
     setTimeout(function() {
       tween.play();
     }, 1000);
-    
+    */
+
+    /* SQUARE DESTROY ANIMATION (SOUCIS D'ORIENTATION DU CARRE)*/ 
+    /* var squareLineTop = new Kinetic.Line({
+      points : [0, 0, 0, 0],
+      stroke:"#FFFFFF",
+      strokeWidth:10
+    });
+    var squareLineRight = new Kinetic.Line({
+      points : [57, 0, 57, 0],
+      stroke:"#FFFFFF",
+      strokeWidth:10
+    });
+    var squareLineBottom = new Kinetic.Line({
+      points : [62, 62, 62, 62],
+      stroke:"#FFFFFF",
+      strokeWidth:10
+    });
+    var squareLineLeft = new Kinetic.Line({
+      points : [0, 67, 0, 67],
+      stroke:"#FFFFFF",
+      strokeWidth:10
+    });
+    this.objectGroup.add(squareLineTop);
+    this.objectGroup.add(squareLineRight);
+    this.objectGroup.add(squareLineBottom);
+    this.objectGroup.add(squareLineLeft);
+
+    var tween4 = new Kinetic.Tween({
+      node : squareLineLeft,
+      points : [0, 67, 0, -5],
+      duration:1,
+      onFinish:thisShape.destroy
+    });
+
+    var tween3 = new Kinetic.Tween({
+      node : squareLineBottom,
+      points : [62, 62, 0, 62],
+      duration:1,
+      onFinish:function() {
+        tween4.play();
+      }
+    });
+
+    var tween2 = new Kinetic.Tween({
+      node : squareLineRight,
+      points : [57, 0, 57, 62],
+      duration:1,
+      onFinish:function() {
+        tween3.play();
+      }
+    });
+
+    var tween = new Kinetic.Tween({
+      node : squareLineTop,
+      points : [0, 0, 62, 0],
+      duration:1,
+      onFinish:function() {
+        tween2.play();
+      }
+    });
+
+    setTimeout(function() {
+      tween.play();
+    }, 1000);
+    */
+
+    /* TRIANGLE DESTROY ANIMATION (SOUCIS DE COINS DU TRIANGLE)*/ 
+    /* var triangleLineRight = new Kinetic.Line({
+      points : [0, -48, 0, -48],
+      stroke:"#FFFFFF",
+      strokeWidth:10
+    });
+    var triangleLineBottom = new Kinetic.Line({
+      points : [42, 24, 42, 24],
+      stroke:"#FFFFFF",
+      strokeWidth:10
+    });
+    var triangleLineLeft = new Kinetic.Line({
+      points : [-42, 24, -42, 24],
+      stroke:"#FFFFFF",
+      strokeWidth:10
+    });
+    this.objectGroup.add(triangleLineLeft);
+    this.objectGroup.add(triangleLineBottom);
+    this.objectGroup.add(triangleLineRight);
+
+    var tween3 = new Kinetic.Tween({
+      node : triangleLineLeft,
+      points : [-42, 24, 0, -48],
+      duration:1,
+      //onFinish:thisShape.destroy
+    });
+
+    var tween2 = new Kinetic.Tween({
+      node : triangleLineBottom,
+      points : [42, 24, -42, 24],
+      duration:1,
+      onFinish:function() {
+        tween3.play();
+      }
+    });
+
+    var tween = new Kinetic.Tween({
+      node : triangleLineRight,
+      points : [0, -48, 42, 24],
+      duration:1,
+      onFinish:function() {
+        tween2.play();
+      }
+    });
+
+    setTimeout(function() {
+      tween.play();
+    }, 1000);*/
   }
   
   this.startDrag = function() {
